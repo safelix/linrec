9b68598ed58aa5510ec80ff12fdb591daf783bfd
Felix Sarnthein <felix.sarnthein@tue.ellis.eu>
2024-09-26 13:52:08 +0200

    add cast_kwarg wrapper

--- linrec/impl/cuda/extension.cpp
+++ linrec/impl/cuda/extension.cpp
@@ -2,15 +2,32 @@
 #include <torch/extension.h>
 #include <linrec.h>
 
-// 
-// https://pybind11.readthedocs.io/en/stable/basics.html#creating-bindings-for-a-simple-function
-// https://pybind11.readthedocs.io/en/stable/advanced/functions.html#accepting-args-and-kwargs
-// https://pybind11.readthedocs.io/en/stable/advanced/pycpp/object.html#casting-back-and-forth
+/* PyTorch uses pybind11 to interface between C++ and Python:
+    - https://pybind11.readthedocs.io/en/stable/basics.html#creating-bindings-for-a-simple-function
+    - https://pybind11.readthedocs.io/en/stable/advanced/functions.html#accepting-args-and-kwargs
+    - https://pybind11.readthedocs.io/en/stable/advanced/pycpp/object.html#casting-back-and-forth
+
+Named arguments and defaults are exposed via the `py::arg("arg")=default` syntax. Keyword arguments
+are exposed via the py::kwargs as the last argument type of C++ function descriptions. For this, we
+wrap the functions in linrec.h to take a py::kwargs argument, cast it to std::map<std::string, int>
+and call the original function. The resulting functions would look like so:
+```
 torch::Tensor linrec_tile_fwd_kwargs(const torch::Tensor &inputs, const torch::Tensor &coeffs, const bool reverse, const py::kwargs& kwargs) {
         return linrec_tile_fwd(inputs, coeffs, reverse, kwargs.cast<std::map<std::string, int>>());
 }
-std::tuple<torch::Tensor, torch::Tensor> linrec_tile_bwd_kwargs(const torch::Tensor &d_outputs, const torch::Tensor &coeffs, const torch::Tensor &outputs, const bool reverse, const py::kwargs& kwargs) {
-        return linrec_tile_bwd(d_outputs, coeffs, outputs, reverse, kwargs.cast<std::map<std::string, int>>());
+``` */
+
+inline auto cast_kwarg(auto& arg) {
+    return arg;
+}
+inline auto cast_kwarg(const py::kwargs& kwarg) {
+    return kwarg.cast<const std::map<std::string, int>>();
+}
+template <typename Ret, typename... Arg>
+inline auto cast_kwarg(Ret (*f)(Arg...)) {
+    return [f](std::conditional_t<std::is_same_v<Arg, const std::map<std::string, int>&>, const py::kwargs&, Arg>... arg) {
+        return f(cast_kwarg(arg)...);
+    };
 }
 
 
@@ -24,14 +41,11 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
                 py::arg("d_outputs"), py::arg("coeffs"), py::arg("outputs"), py::arg("reverse")=false);
 
     
-    m.def("linrec_tile_fwd", torch::wrap_pybind_function(linrec_tile_fwd_kwargs), 
+    m.def("linrec_tile_fwd", torch::wrap_pybind_function(cast_kwarg(linrec_tile_fwd)), 
                 "Parallel CUDA implementation of linear recursion forward pass.",
                 py::arg("inputs"), py::arg("coeffs"), py::arg("reverse")=false);
-    //m.def("linrec_tile_fwd", torch::wrap_pybind_function(linrec_tile_fwd), 
-    //            "Parallel CUDA implementation of linear recursion forward pass.",
-    //            py::arg("inputs"), py::arg("coeffs"), py::arg("reverse")=false, py::arg("options")=std::map<std::string, int>());       
- 
-    m.def("linrec_tile_bwd", torch::wrap_pybind_function(linrec_tile_bwd_kwargs), 
+
+    m.def("linrec_tile_bwd", torch::wrap_pybind_function(cast_kwarg(linrec_tile_bwd)), 
                 "Parallel CUDA implementation of linear recursion backward pass.",
                 py::arg("d_outputs"), py::arg("coeffs"), py::arg("outputs"), py::arg("reverse")=false);
 }

0cbc46189eed984b7a14546a5c942341ab898896
Felix Sarnthein <felix.sarnthein@tue.ellis.eu>
2024-11-01 21:50:24 +0100

    cuops: add wrappers for _C methods

--- linrec/impl/cuda/ops.py
+++ linrec/impl/cuda/ops.py
@@ -6,12 +6,22 @@ except ImportError:
    from .build import extension
    _C = extension()
    
-__all__ = ["linrec_ref", "linrec_tile"]
+__all__ = ["linrec_ref", "linrec_tile", "linrec_pipe"]
+config_names = _C.config_names
+config_list = list({tuple(c):None for c in _C.config_list}) # make config_list unique using ordered dict
+
+
+# CUDA Reference Implementations
+def linrec_ref_fwd(inputs:torch.Tensor, coeffs:torch.Tensor, reverse:bool=False):
+    return _C.linrec_ref_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse)
+
+def linrec_ref_bwd(d_outputs:torch.Tensor, coeffs:torch.Tensor, outputs:torch.Tensor, reverse:bool=False):
+    return _C.linrec_ref_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=reverse)
 
 class LinrecRefFn(torch.autograd.Function):
     @staticmethod
     def forward(ctx:FunctionCtx, inputs:torch.Tensor, coeffs:torch.Tensor, reverse:bool=False) -> torch.Tensor:
-        outputs = _C.linrec_ref_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse)
+        outputs = linrec_ref_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse)
         ctx.save_for_backward(coeffs, outputs)
         ctx.reverse = reverse
         return outputs
@@ -19,17 +29,28 @@ class LinrecRefFn(torch.autograd.Function):
     @staticmethod
     def backward(ctx:FunctionCtx, d_outputs:torch.Tensor):
         coeffs, outputs = ctx.saved_tensors
-        d_inputs, d_coeffs = _C.linrec_ref_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=ctx.reverse)
+        d_inputs, d_coeffs = linrec_ref_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=ctx.reverse)
         return d_inputs, d_coeffs, None
 
 def linrec_ref(inputs:torch.Tensor, coeffs:torch.Tensor, reverse=False):
     return LinrecRefFn.apply(inputs, coeffs, reverse)
 
 
+
+# CUDA Tiled Implementations
+def linrec_tile_fwd(inputs:torch.Tensor, coeffs:torch.Tensor, reverse:bool=False, **config):
+    return _C.linrec_tile_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse, **config)
+
+def linrec_tile_bwd(d_outputs:torch.Tensor, coeffs:torch.Tensor, outputs:torch.Tensor, reverse:bool=False, **config):
+    return _C.linrec_tile_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=reverse, **config)
+
+def linrec_tile_attrs(fwd:bool, **config):
+    return _C.linrec_tile_attrs(fwd, **config)
+
 class LinrecTileFn(torch.autograd.Function):
     @staticmethod
     def forward(ctx:FunctionCtx, inputs:torch.Tensor, coeffs:torch.Tensor, reverse:bool=False) -> torch.Tensor:
-        outputs = _C.linrec_tile_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse)
+        outputs = linrec_tile_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse)
         ctx.save_for_backward(coeffs, outputs)
         ctx.reverse = reverse
         return outputs
@@ -37,17 +58,28 @@ class LinrecTileFn(torch.autograd.Function):
     @staticmethod
     def backward(ctx:FunctionCtx, d_outputs:torch.Tensor):
         coeffs, outputs = ctx.saved_tensors
-        d_inputs, d_coeffs = _C.linrec_tile_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=ctx.reverse)
+        d_inputs, d_coeffs = linrec_tile_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=ctx.reverse)
         return d_inputs, d_coeffs, None
 
 def linrec_tile(inputs:torch.Tensor, coeffs:torch.Tensor, reverse=False):
     return LinrecTileFn.apply(inputs, coeffs, reverse)
 
 
+
+# CUDA Piped Implementations
+def linrec_pipe_fwd(inputs:torch.Tensor, coeffs:torch.Tensor, reverse:bool=False, **config):
+    return _C.linrec_pipe_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse, **config)
+
+def linrec_pipe_bwd(d_outputs:torch.Tensor, coeffs:torch.Tensor, outputs:torch.Tensor, reverse:bool=False, **config):
+    return _C.linrec_pipe_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=reverse, **config)
+
+def linrec_pipe_attrs(fwd:bool, **config):
+    return _C.linrec_pipe_attrs(fwd, **config)
+
 class LinrecPipeFn(torch.autograd.Function):
     @staticmethod
     def forward(ctx:FunctionCtx, inputs:torch.Tensor, coeffs:torch.Tensor, reverse:bool=False) -> torch.Tensor:
-        outputs = _C.linrec_pipe_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse)
+        outputs = linrec_pipe_fwd(inputs=inputs, coeffs=coeffs, reverse=reverse)
         ctx.save_for_backward(coeffs, outputs)
         ctx.reverse = reverse
         return outputs
@@ -55,7 +87,7 @@ class LinrecPipeFn(torch.autograd.Function):
     @staticmethod
     def backward(ctx:FunctionCtx, d_outputs:torch.Tensor):
         coeffs, outputs = ctx.saved_tensors
-        d_inputs, d_coeffs = _C.linrec_pipe_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=ctx.reverse)
+        d_inputs, d_coeffs = linrec_pipe_bwd(d_outputs=d_outputs, coeffs=coeffs, outputs=outputs, reverse=ctx.reverse)
         return d_inputs, d_coeffs, None
 
 def linrec_pipe(inputs:torch.Tensor, coeffs:torch.Tensor, reverse=False):

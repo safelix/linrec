69ca6e9c7d530eb164174295ed9c0c330199a8ae
Felix Sarnthein <felix.sarnthein@tue.ellis.eu>
2024-11-06 21:05:07 +0100

    executable.cpp: update to linrec_pipe

--- linrec/impl/cuda/executable.cpp
+++ linrec/impl/cuda/executable.cpp
@@ -19,11 +19,11 @@ int main(int argc, char *argv[]) {
 
     // Minimal Parser
     std::vector<std::string> args(argv, argv + argc);
-    int numseq = parse(args, "--numseq", 14200);
-    int seqlen = parse(args, "--seqlen", 4*1024);
+    int numseq = parse(args, "--numseq", 142*100);
+    int seqlen = parse(args, "--seqlen", 65536);
     bool reverse = parse(args, "--reverse", false);
     int device_id = parse(args, "--device", 0);
-    auto linrec_options = std::map<std::string, int>{
+    auto config = std::map<std::string, int>{
         {"kMaxElemsPerThread", parse(args, "--kMaxElemsPerThread", 4)},
         {"kMaxThreadsPerBlock", parse(args, "--kMaxThreadsPerBlock", 1024)},
         {"memcode", parse(args, "--memcode", 0)},
@@ -37,30 +37,33 @@ int main(int argc, char *argv[]) {
     auto device = (device_id == -1) ? Device("cpu") : Device(DeviceType::CUDA, device_id);
     auto options = TensorOptions().dtype(dtype).device(device).requires_grad(false);
     
-    // Forward
     Tensor inputs = torch::randn(shape, options);
-    Tensor coeffs = torch::exp(-torch::rand(shape, options));
+    Tensor coeffs = torch::rand(shape, options);
+    Tensor d_outputs = torch::randn(shape, options);
     std::cout << "inputs: " << inputs.toString() << inputs.sizes() << std::endl << std::flush;
     std::cout << "coeffs: " << coeffs.toString() << coeffs.sizes() << std::endl << std::flush;
-
-    Tensor solution, outputs; 
-    
-    solution = linrec_ref_fwd(inputs, coeffs, reverse);
-    if (linrec_options.at("algocode") == 0)
-        solution = inputs;
-    outputs = linrec_tile_fwd(inputs, coeffs, reverse, linrec_options);
-    std::cout << "outputs: " << outputs.toString() << outputs.sizes();
-    std::cout << ", error=" << (solution -  outputs).abs().max().item() << std::endl << std::flush;
-    
-    // Backward
-    Tensor d_outputs = torch::randn(shape, options);
     std::cout << "d_outputs: " << d_outputs.toString() << d_outputs.sizes() << std::endl << std::flush;
 
-    /*
-    auto[d_inputs, d_coeffs] = linrec_ref_bwd(d_outputs, coeffs, outputs);    // structured binding
-    std::cout << "d_inputs: " << d_inputs.toString() << d_inputs.sizes() << std::endl;
-    std::cout << "d_coeffs: " << d_coeffs.toString() << d_coeffs.sizes() << std::endl;
-    */
+    // Reference implementations
+    Tensor outputs_ref = linrec_ref_fwd(inputs, coeffs, reverse);
+    auto[d_inputs_ref, d_coeffs_ref] = linrec_ref_bwd(d_outputs, coeffs, outputs_ref);    // structured binding
 
-    //TupleTensor outputs = linrec_tile_bwd(inputs, coeffs);
+    // Forward implementation 
+    Tensor outputs = linrec_pipe_fwd(inputs, coeffs, reverse, config);
+    std::cout << "linrec_pipe_fwd:";
+    std::cout << " reg=" << linrec_pipe_attrs(true, config)["numRegs"];
+    std::cout << ", lmem=" << linrec_pipe_attrs(true, config)["localSizeBytes"] << std::endl;
+    std::cout << "outputs: " << outputs.toString() << outputs.sizes();
+    std::cout << ", error=" << (outputs_ref -  outputs).abs().max().item() << std::endl << std::flush;
+    
+    // Backward implementations
+    auto[d_inputs, d_coeffs] = linrec_pipe_bwd(d_outputs, coeffs, outputs, reverse, config);
+    std::cout << "linrec_pipe_bwd:";
+    std::cout << " reg=" << linrec_pipe_attrs(false, config)["numRegs"];
+    std::cout << ", lmem=" << linrec_pipe_attrs(false, config)["localSizeBytes"] << std::endl;
+    std::cout << "d_inputs: " << d_inputs.toString() << d_inputs.sizes();
+    std::cout << ", error=" << (d_inputs_ref -  d_inputs).abs().max().item() << std::endl << std::flush;
+    std::cout << "d_coeffs: " << d_coeffs.toString() << d_coeffs.sizes();
+    std::cout << ", error=" << (d_coeffs_ref -  d_coeffs).abs().max().item() << std::endl << std::flush;
+
 }
